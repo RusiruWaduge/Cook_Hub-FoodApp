2b5a20b79f1284c56dedf3064080e7e14d4a2829
RusiruWaduge <156440372+RusiruWaduge@users.noreply.github.com>
2025-04-28 02:08:12 +0530

    initial commit

--- Backend/src/main/java/com/example/backend/repository/CommunityPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.CommunityPost;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface CommunityPostRepository extends MongoRepository<CommunityPost, String> {
-    List<CommunityPost> findByCommunityId(String communityId);
-}
